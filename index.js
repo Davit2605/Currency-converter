2c5f06e1d6248c15c33ca9a7f59905fa851cb437
Davit Machurishvili <125227660+Davit2605@users.noreply.github.com>
2025-01-30 22:20:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const worthFirstEl = document.getElementById("worth-first");
 const worthSecondEl = document.getElementById("worth-second");
 const exchangeRateEl = document.getElementById("exchange-rate");
 getRate();
-function getRate() {
+functio getRate() {
   fetch(
     `https://v6.exchangerate-api.com/v6/bf9de5ed704785029475402e/latest/${currencyFirstEl.value}`
   )
